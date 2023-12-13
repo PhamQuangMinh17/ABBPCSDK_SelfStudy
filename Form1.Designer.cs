3a6f28f41a3992006ec1707bdc74ec5337eaeba6
Pham Quang Minh <107908867+PhamQuangMinh17@users.noreply.github.com>
2023-12-13 17:41:17 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace PCSDK_Exp_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.IPAddress = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Availability = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Virtual = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SystemName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.RobotWareVersion = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ControllerName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.IPAddress,
+            this.ID,
+            this.Availability,
+            this.Virtual,
+            this.SystemName,
+            this.RobotWareVersion,
+            this.ControllerName});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(13, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(810, 194);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // IPAddress
+            // 
+            this.IPAddress.Text = "IPAddress";
+            this.IPAddress.Width = 123;
+            // 
+            // ID
+            // 
+            this.ID.Text = "ID";
+            this.ID.Width = 101;
+            // 
+            // Availability
+            // 
+            this.Availability.Text = "Availability";
+            this.Availability.Width = 76;
+            // 
+            // Virtual
+            // 
+            this.Virtual.Text = "Virtual";
+            this.Virtual.Width = 73;
+            // 
+            // SystemName
+            // 
+            this.SystemName.Text = "System Name";
+            this.SystemName.Width = 140;
+            // 
+            // RobotWareVersion
+            // 
+            this.RobotWareVersion.Text = "RobotWare Version";
+            this.RobotWareVersion.Width = 113;
+            // 
+            // ControllerName
+            // 
+            this.ControllerName.Text = "Controller Name";
+            this.ControllerName.Width = 186;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(13, 212);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(809, 217);
+            this.panel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 71);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Rapid Program";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(834, 441);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Network scanning window";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader IPAddress;
+        private System.Windows.Forms.ColumnHeader ID;
+        private System.Windows.Forms.ColumnHeader Availability;
+        private System.Windows.Forms.ColumnHeader Virtual;
+        private System.Windows.Forms.ColumnHeader SystemName;
+        private System.Windows.Forms.ColumnHeader RobotWareVersion;
+        private System.Windows.Forms.ColumnHeader ControllerName;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,176 @@
+﻿using ABB.Robotics.Controllers;
+using ABB.Robotics.Controllers.Discovery;
+using ABB.Robotics.Controllers.RapidDomain;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace PCSDK_Exp_1
+{
+    public partial class Form1 : Form
+    {
+        // Local Variables 
+        private Controller controller = null;
+        private ABB.Robotics.Controllers.RapidDomain.Task[] tasks = null;
+        private NetworkScanner scanner = null;
+        private NetworkWatcher networkwatcher = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Initializing network scanner and watcher
+            NetworkScanning();
+            SetupNetworkWatcher();
+        }
+
+        /* Scan the network as soon as the application start */
+        private void NetworkScanning()
+        {
+            /* Scan Network for Controllers*/
+            this.scanner = new NetworkScanner();
+            this.scanner.Scan();
+            ControllerInfoCollection controllers = scanner.Controllers;
+
+            /* Add discovered Controllers t ListView */
+            ListViewItem item = null; // Create empty listview
+            foreach (ControllerInfo controllerInfo in controllers)
+            {
+                item = new ListViewItem(controllerInfo.IPAddress.ToString());
+                item.SubItems.Add(controllerInfo.Id);
+                item.SubItems.Add(controllerInfo.Availability.ToString());
+                item.SubItems.Add(controllerInfo.IsVirtual.ToString());
+                item.SubItems.Add(controllerInfo.SystemName);
+                item.SubItems.Add(controllerInfo.Version.ToString());
+                item.SubItems.Add(controllerInfo.ControllerName);
+
+                listView1.Items.Add(item);
+                item.Tag = controllerInfo;
+            }
+        }
+
+        /* Initialize Network Watcher:
+         the application can supervise the network and detect when controllers are lost or added.
+        */
+        private void SetupNetworkWatcher()
+        {
+            // Initialize NetworkWatcher
+            this.networkwatcher = new NetworkWatcher(scanner.Controllers);
+
+            // Subscribe to Found and Lost events
+            this.networkwatcher.Found += new EventHandler<NetworkWatcherEventArgs>(HandleFoundEvent);
+            this.networkwatcher.Lost += new EventHandler<NetworkWatcherEventArgs>(HandleLostEvent);
+
+            // Enable raising events
+            this.networkwatcher.EnableRaisingEvents = true;
+        }
+
+        /* Handle event */
+        /*  the events will be received on a background thread and should result in an update of the user interface 
+         *   --> The Invoke method must be called in the event handler
+         *  */
+        void HandleFoundEvent(object sender, NetworkWatcherEventArgs e)
+        {
+            this.Invoke(new EventHandler<NetworkWatcherEventArgs>(AddControllerToListView),
+                new Object[] { this, e });
+        }
+
+        /*  Event handler AddControllerToListView updates the user interface */
+        private void AddControllerToListView(object sender, NetworkWatcherEventArgs e)
+        {
+            ControllerInfo controllerInfo = e.Controller;
+            ListViewItem item = new ListViewItem(controllerInfo.IPAddress.ToString());
+            item.SubItems.Add(controllerInfo.Id);
+            item.SubItems.Add(controllerInfo.Availability.ToString());
+            item.SubItems.Add(controllerInfo.IsVirtual.ToString());
+            item.SubItems.Add(controllerInfo.SystemName);
+            item.SubItems.Add(controllerInfo.Version.ToString());
+            item.SubItems.Add(controllerInfo.ControllerName);
+            this.listView1.Items.Add(item);
+            item.Tag = controllerInfo;
+        }
+
+        private void HandleLostEvent(object sender, NetworkWatcherEventArgs e)
+        {
+            // Handle the event when a controller is lost
+            ControllerInfo lostController = e.Controller;
+            // Update your UI or perform any required action
+            // For example, removing the lost controller from the ListView
+            foreach (ListViewItem item in listView1.Items)
+            {
+                if (item.SubItems[0].Text == lostController.IPAddress.ToString())
+                {
+                    listView1.Invoke(new Action(() => { listView1.Items.Remove(item); }));
+                    break;
+                }
+            }
+        }
+
+        /* ListView - DoubleClick event handler */
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            ListViewItem item = this.listView1.SelectedItems[0];
+            if (item.Tag != null)
+            {
+                ControllerInfo controllerInfo = (ControllerInfo)item.Tag;
+                // Availability.Available is replaced by  ABB.Robotics.Controllers.Availability.Available
+                if (controllerInfo.Availability == ABB.Robotics.Controllers.Availability.Available)
+                {
+                    if (this.controller != null)
+                    {
+                        this.controller.Logoff();
+                        this.controller.Dispose();
+                        this.controller = null;
+                    }
+                    this.controller = ControllerFactory.CreateFrom(controllerInfo);
+                    this.controller.Logon(UserInfo.DefaultUser);
+                }
+                else
+                {
+                    MessageBox.Show("Selected controller not available.");
+                }
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (controller.OperatingMode == ControllerOperatingMode.Auto)
+                {
+                    tasks = controller.Rapid.GetTasks();
+                    using (Mastership m = Mastership.Request(controller.Rapid))
+                    {
+                        //Perform operation
+                        tasks[0].Start();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show(
+                        "Automatic mode is required to start execution from a remote client.");
+                }
+            }
+            catch (System.InvalidOperationException ex)
+            {
+                MessageBox.Show("Mastership is held by another client." + ex.Message);
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show("Unexpected error occurred: " + ex.Message);
+            }
+        }
+    }
+}
